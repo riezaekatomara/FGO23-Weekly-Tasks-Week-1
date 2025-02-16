ba9710a841224ae7ec1ea3f750b90e7c9fa8decb
riezaekatomara <130181217+riezaekatomara@users.noreply.github.com>
2025-02-16 10:58:13 +0700

    Weekly Task Week 1 FGO23 Task Javascript Introduc1

---
+++ task-javascript-introduction-1.js
@@ -0,0 +1,15 @@
+let product = {
+    name: "Laptop Infinix XYZ",
+    rating: 4.9,
+    price: 5500000,
+    isActive: true,
+    image: ["image1.png", "image2.png", "image3.png"],
+    description: "Laptop canggih dengan fitur terbaru",
+    variants: {
+        name: "Color Variants",
+        option: ["Green", "Navy", "Grey"]
+    },
+    storeName: "AR7 Tech Store"
+}
+
+console.log(product);
